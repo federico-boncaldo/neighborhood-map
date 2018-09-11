99b3d8b2ee9b48c03a3ff124e6fd597c5ab9e36e
Federico <federico.boncaldo@gmail.com>
2018-09-11 09:51:35 +0100

    Add colours to markers and mouseover effect
    The markers have two different icons, the default one and another one
    for mouseover and click events.

--- js/map.js
+++ js/map.js
@@ -6,22 +6,59 @@ function initMap() {
 		center: {lat: 53.145472, lng: -6.064194 },
 		zoom: 11
 	});
-
-	// displayMarkers(places);
 }
 
+/**
+ * [createMarkers description]
+ * @return {[type]} [description]
+ */
 function createMarkers(){
 	let markers = []
 
+	let defaultIcon = makeMarkerIcon('00ffff');
+	let highlightedIcon = makeMarkerIcon('f0ffff');
+
 	places.forEach(function(place) {
-		markers.push(new google.maps.Marker({
+		let marker = new google.maps.Marker({
 			position: place.location,
 			title: place.name,
-			animation: google.maps.Animation.DROP
-		}));
+			animation: google.maps.Animation.DROP,
+			icon: defaultIcon
+		});
+
+		marker.addListener('click', function(){
+			this.setIcon(highlightedIcon);
+			// populateInfoWindow();
+		});
+
+		marker.addListener('mouseover', function(){
+			this.setIcon(highlightedIcon)
+		});		
+
+		marker.addListener('mouseout', function(){
+			this.setIcon(defaultIcon)
+		});
+
+		markers.push(marker);
 	});
 
 	return markers;
+}
+
+/**
+ * [makeMarkerIcon description]
+ * @param  {[type]} markerColor [description]
+ * @return {[type]}             [description]
+ */
+function makeMarkerIcon(markerColor) {
+let markerImage = new google.maps.MarkerImage(
+  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+  '|40|_|%E2%80%A2',
+  new google.maps.Size(21, 34),
+  new google.maps.Point(0, 0),
+  new google.maps.Point(10, 34),
+  new google.maps.Size(21,34));
+return markerImage;
 }
 
 /**
