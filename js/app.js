af56a0024c980f086da8147a3cc7d4f7fd6198d4
Federico Boncaldo <federico@cryptolab.net>
2018-10-01 18:26:38 +0100

    chore: Add comments and cleaning to js files

--- js/app.js
+++ js/app.js
@@ -1,90 +1,107 @@
-
-//list of styles
-let styles = [];
-
-//list of places
 let places = [
-      {name: 'Wicklow Mountains', location: {lat: 53.081805, lng: -6.393859}},
-      {name: 'Glendalough', location: {lat: 53.00363, lng: -6.36264}},
-      {name: 'Powerscourt House & Gardens', location: {lat: 53.184251, lng: -6.186633}},
-      {name: 'Victor\'s Way Sculpture Garden', location: {lat: 53.085444, lng: -6.219073}},
-      {name: 'The Happy Pear', location: {lat:  53.145472, lng: -6.064194 }},
+      {
+        name: "Wicklow Mountains",
+        location: {lat: 53.081805, lng: -6.393859}
+    },
+      {
+        name: "Glendalough",
+        location: {lat: 53.00363, lng: -6.36264}
+    },
+      {
+        name: "Powerscourt House & Gardens",
+        location: {lat: 53.184251, lng: -6.186633}
+    },
+      {
+        name: "Victor's Way Sculpture Garden",
+        location: {lat: 53.085444, lng: -6.219073}
+    },
+      {
+        name: "The Happy Pear",
+        location: {lat:  53.145472, lng: -6.064194 }
+    }
 ];
 
 /**
- * Model and logic of the application
- * @param data the place we want to manage
- * @param { } [varname] [description]
- * @param { } [varname] [description]
+ * @description Model and logic of the application
+ * @constructor
+ * @param string name location name
+ * @param boolean display used to display the location during the search
+ * @param boolean favorite used to pick favorite locations
  */
 let Place = function(name, display, favorite) {
-	let self = this;
-	self.name = ko.observable(name);
-	self.display = ko.observable(display);
-	self.favorite = ko.observable(favorite);
-}
+    let self = this;
+    self.name = ko.observable(name);
+    self.display = ko.observable(display);
+    self.favorite = ko.observable(favorite);
+};
 
 /**
- * 
+ * @description Allow the acces to places list and markers.
+ * Provide the search and animateMarker features.
+ * @constructor
  */
 let ViewModel = function() {
-	let self = this;
+    let self = this;
 
-	self.placesList = ko.observableArray([]);
-	self.markers = createMarkers();
-	
-	places.forEach(function(place) {
-		self.placesList.push(new Place(place.name, true, false));
-	});
+    self.placesList = ko.observableArray([]);
+    self.markers = createMarkers();
 
-	self.placesList.sort();
+    places.forEach(function(place) {
+        self.placesList.push(new Place(place.name, true, false));
+    });
 
-	//the array of places is passed by providing the content of placesList observable array
-	displayMarkers(self.markers, self.placesList());
+    self.placesList.sort();
 
-	self.query = ko.observable("");
-	/**
-	 * Display only the places that have the query string which matches 
-	 * part of the title or the entire title.
-	 */
-	self.search = function() {
-		hideMarkers(self.markers);
+    //the array of places is passed by providing the content of placesList
+    //observable array
+    displayMarkers(self.markers, self.placesList());
 
-		self.placesList().forEach(function(place){
-			place.display(false)
-			if(place.name().toLowerCase().indexOf(self.query().toLowerCase()) >= 0){
-				place.display(true);
-			}
-		});
+    self.query = ko.observable("");
+    /**
+     * @description Display only the places that have the query string which
+     * matches
+     * part of the title or the entire title.
+     */
+    self.search = function() {
+        hideMarkers(self.markers);
 
-		displayMarkers(self.markers, self.placesList());
+        self.placesList().forEach(function(place){
+            place.display(false);
+            if(
+                place.name().toLowerCase().indexOf(self.query().toLowerCase())
+                >= 0
+            ){
+                place.display(true);
+            }
+        });
 
-	};
+        displayMarkers(self.markers, self.placesList());
+
+    };
 
 
-	/**
-	 * Animate the markes which is clicked
-	 * @param  {[type]} data  [description]
-	 * @param  {[type]} event [description]
-	 * @return {[type]}       [description]
-	 */
-	self.animateMarker = function(data, event) {
-		let found = false;
-		self.markers.forEach(function(marker){
-			marker.setIcon(makeMarkerIcon('00ffff'));
-			marker.setAnimation(null);
-			if(marker.title == data.name()){
-				marker.setIcon(makeMarkerIcon('f0ffff'));
-				marker.setAnimation(google.maps.Animation.BOUNCE);
-				populateInfoWindow(marker);
-				found = true;
-			}
-		});
+    /**
+     * @description Animate the marker of the location
+     * @param  observable data the location selected by the user
+     */
+    self.animateMarker = function(data) {
+        let found = false;
+        self.markers.forEach(function(marker){
+            marker.setIcon(makeMarkerIcon("00ffff"));
+            marker.setAnimation(null);
+            if(marker.title == data.name()){
+                marker.setIcon(makeMarkerIcon("f0ffff"));
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                populateInfoWindow(marker);
+                found = true;
+            }
+        });
 
-		if(!found)
-			alert("Marker for " + data.name() + "not found");
-	};
+        if(!found) {
+            alert("Marker for " + data.name() + "not found");
+        }
+    };
 
-}
+};
 
 ko.applyBindings(new ViewModel());
--- js/map.js
+++ js/map.js
@@ -1,164 +1,174 @@
 let map;
+let infowindow;
 
-let infowindow; 
-
+/**
+ * @description Callback function, create the map and the infowindow object
+ */
 function initMap() {
 
-	infowindow = new google.maps.InfoWindow();
-	
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 53.085444, lng: -6.219073 },
-		zoom: 9.5
-	});
-}
+  infowindow = new google.maps.InfoWindow();
 
-function mapError() {
-  //add a class to the map div in order display a different message and style the page differently
-  let mapDiv = document.getElementById('map');
-  mapDiv.className = "no-map";
-  mapDiv.innerHTML = "There was a problem while downloading the map.<br> We're sorry for the inconvenience.";
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 53.085444, lng: -6.219073 },
+    zoom: 9.5
+  });
 }
 
 /**
- * [createMarkers description]
- * @return {[type]} [description]
+ * @description Error function in case of issues during map loading
+ */
+function mapError() {
+  //add a class to the map div in order display a different message and style
+  //the page differently
+  let mapDiv = document.getElementById("map");
+  mapDiv.className = "no-map";
+  mapDiv.innerHTML = "There was a problem while downloading the map.<br> " +
+    "We're sorry for the inconvenience.";
+}
+
+/**
+ * @description Add one marker for each location and store it in an array
+ * @return array All the location markers
  */
 function createMarkers(){
-	let markers = [];
+  let markers = [];
 
-	const defaultIcon = makeMarkerIcon('00ffff');
-	const highlightedIcon = makeMarkerIcon('f0ffff');
+  const defaultIcon = makeMarkerIcon("00ffff");
+  const highlightedIcon = makeMarkerIcon("f0ffff");
 
-	places.forEach(function(place) {
-		let marker = new google.maps.Marker({
-			position: place.location,
-			title: place.name,
-			animation: google.maps.Animation.DROP,
-			icon: defaultIcon
-		});
+  places.forEach(function(place) {
+    let marker = new google.maps.Marker({
+      position: place.location,
+      title: place.name,
+      animation: google.maps.Animation.DROP,
+      icon: defaultIcon
+    });
 
-		markers.push(marker);
+    markers.push(marker);
 
-		marker.addListener('click', function(){
-			this.setIcon(highlightedIcon);
-      this.setAnimation(google.maps.Animation.BOUNCE);
-			populateInfoWindow(this);
-		});
+    marker.addListener("click", function(){
+      marker.setIcon(highlightedIcon);
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      populateInfoWindow(marker);
+    });
 
-		marker.addListener('mouseover', function(){
-			this.setIcon(highlightedIcon);
-		});		
+    marker.addListener("mouseover", function(){
+      marker.setIcon(highlightedIcon);
+    });
 
-		marker.addListener('mouseout', function(){
-			this.setIcon(defaultIcon);
-      this.setAnimation(null);
-		});
+    marker.addListener("mouseout", function(){
+      marker.setIcon(defaultIcon);
+      marker.setAnimation(null);
+    });
 
-	});
+  });
 
-	return markers;
+  return markers;
 }
 
 /**
- * [makeMarkerIcon description]
- * @param  {[type]} markerColor [description]
- * @return {[type]}             [description]
+ * @description Modify the marker image according to the color provided
+ * @param  string markerColor the color code that will be used
+ * @return object markerImage the modified image of the marker
  */
 function makeMarkerIcon(markerColor) {
-let markerImage = new google.maps.MarkerImage(
-  'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-  '|40|_|%E2%80%A2',
-  new google.maps.Size(21, 34),
-  new google.maps.Point(0, 0),
-  new google.maps.Point(10, 34),
-  new google.maps.Size(21,34));
-return markerImage;
+  let markerImage = new google.maps.MarkerImage(
+    "http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" +
+    markerColor + "|40|_|%E2%80%A2",
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
 }
 
 /**
- * [displayMarkers description]
- * @param  {[type]} places [description]
- * @return {[type]}        [description]
+ * @description Show the markers on the map according to the display status of
+ * the place
+ * @param  array markers
+ * @param  array placesList
+ * @return array markers markers to display
  */
 function displayMarkers(markers, placesList) {
-	
-	// let bounds = new google.maps.LatLngBounds();
 
-	placesList.forEach(function(place){
-		markers.forEach(function(marker){
-			if(
-          place.name() == marker.title && 
+  // let bounds = new google.maps.LatLngBounds();
+
+  placesList.forEach(function(place){
+    markers.forEach(function(marker){
+      if(
+          place.name() === marker.title &&
           place.display()
-        )
-				marker.setMap(map);
-			// bounds.extend(marker.position);
-		});
-	});
+        ) {
+        marker.setMap(map);
+      }
+      // bounds.extend(marker.position);
+    });
+  });
 
-	// map.fitBounds(bounds);
-	return markers
+  // map.fitBounds(bounds);
+  return markers;
 }
 
 /**
- * [hideMarkers description]
- * @return {[type]} [description]
+ * @description Hide all markers on the map
  */
 function hideMarkers(markers){
-	
-	markers.forEach(function(marker){
-		marker.setMap(null);
-	});
+
+  markers.forEach(function(marker){
+    marker.setMap(null);
+  });
 }
 
 /**
- * [populateInfoWindow description]
- * @param  {[type]} marker     [description]
- * @return {[type]}            [description]
+ * @description Add panorama of the location and wiki article to the infowindow
+ * marker and display it. Otherwise inform the user of the absence of the
+ * street view image
+ * @param object marker
  */
 function populateInfoWindow(marker) {
     // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
+    if (infowindow.marker !== marker) {
       // Clear the infowindow content to give the streetview time to load.
       infowindow.marker = marker;
       // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick', function() {
+      infowindow.addListener("closeclick", function() {
         infowindow.marker = null;
       });
-      var streetViewService = new google.maps.StreetViewService();
-      var radius = 50;
+      let streetViewService = new google.maps.StreetViewService();
+      let radius = 50;
 
-      infowindow.setContent('<h3>' + marker.title + '</h3>' + 
-      '<div id="pano"></div>' +
-      '<div id="wikipedia-link"></div>');
+      infowindow.setContent("<h3>" + marker.title + "</h3>" +
+      "<div id='pano'></div> <div id='wikipedia-link'></div>");
 
       // In case the status is OK, which means the pano was found, compute the
-      // position of the streetview image, then calculate the heading, then get a
-      // panorama from that and set the options
+      // position of the streetview image, then calculate the heading, then get
+      // a panorama from that and set the options
       function getStreetView(data, status) {
-        let pano = document.getElementById('pano');
-        if (status == google.maps.StreetViewStatus.OK) {
-          var nearStreetViewLocation = data.location.latLng;
-          var heading = google.maps.geometry.spherical.computeHeading(
+        let pano = document.getElementById("pano");
+        if (status === google.maps.StreetViewStatus.OK) {
+          let nearStreetViewLocation = data.location.latLng;
+          let heading = google.maps.geometry.spherical.computeHeading(
             nearStreetViewLocation, marker.position);
 
-            var panoramaOptions = {
+            let panoramaOptions = {
               position: nearStreetViewLocation,
               pov: {
                 heading: heading,
                 pitch: 30
               }
             };
-          var panorama = new google.maps.StreetViewPanorama(
+          let panorama = new google.maps.StreetViewPanorama(
             pano, panoramaOptions);
         } else {
-          pano.innerHTML = 'No Street View Found';
-          pano.className = 'no-pano';
+          pano.innerHTML = "No Street View Found";
+          pano.className = "no-pano";
         }
       }
 
       // Use streetview service to get the closest streetview image within
       // 50 meters of the markers position
-      streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+      streetViewService.getPanoramaByLocation(
+        marker.position, radius, getStreetView);
 
       // Open the infowindow on the correct marker.
       infowindow.open(map, marker);
@@ -168,16 +178,18 @@ function populateInfoWindow(marker) {
 }
 
 /**
- * [getWikipediaLinks description]
- * @param  {[type]} title [description]
- * @return {[type]}       [description]
+ * @description Display one wikipedia article for each location if present
+ * inside the infowindow
+ * @param string title location name
  */
 function getWikipediaLinks(title){
 
-  let $wikiLink = $('#wikipedia-link');
-  
+  let $wikiLink = $("#wikipedia-link");
+
   //the search will retrieve only one article max. for each location
-  let wikipediaURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ title +"&limit=1&profile=normal&format=json&callback=wikiCallBack" ;
+  let wikipediaURL = "https://en.wikipedia.org/w/api.php?" +
+  "action=opensearch&search=" + title +
+  "&limit=1&profile=normal&format=json&callback=wikiCallBack" ;
 
   let wikiRequestTimeout = setTimeout(function(){
       $wikiLink.text("Failed to get wikipedia article");
@@ -188,18 +200,20 @@ function getWikipediaLinks(title){
     dataType: "jsonp",
     success: function(response){
       let articleList = response[1];
-      (articleList.length != 0) ? $wikiLink.append("<h4>Wikipedia article</h4>") : $wikiLink.text("Wikipedia article not present");
+      (articleList.length !== 0) ?
+        $wikiLink.append("<h4>Wikipedia article</h4>") :
+        $wikiLink.text("Wikipedia article not present");
 
-      for(let article of articleList){
+      articleList.forEach(function(article){
         let url = "https://en.wikipedia.org/wiki/" + article;
-        $wikiLink.append('<li><a href="' + url + '">' +
-            article + '</a></li>');
-      }
+        $wikiLink.append("<li><a href='" + url + "'>" +
+            article + "</a></li>");
+      });
 
       clearTimeout(wikiRequestTimeout);
     }
   }).fail(function(jqXHR, textStatus){
-    $wikiLink.text("Wikipedia request failed: " + textStatus + " " + jqXHR.status );
-    console.log(jqXHR);
+    $wikiLink.text("Wikipedia request failed: " + textStatus +
+      " " + jqXHR.status );
   });
 }
